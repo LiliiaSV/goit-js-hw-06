ab57ac5a26929d895dc7e34f5442d480956571b8
Liliia <sachenkoliliia@gmail.com>
2023-03-23 13:10:14 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,11 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-input.addEventListener("input", (event) =>{
+input.addEventListener("input", InputChange);
+
+function InputChange(event) {
     output.textContent = event.currentTarget.value;
-    if (output.textContent === " ") {
-        output.textContent = 'Anonymous'
-    };
-});
+    if (event.currentTarget.value.trim() === '') {
+        output.textContent = 'Anonymous';
+    }
+}
